998d3401c19e76f57c52f8c4ff5b99ac3ba83cbc
SaiManideep00 <saimanideep00@gmail.com>
2024-12-04 23:51:08 -0500

    Added Xray Tracing between microservices

--- mq-service/src/main/java/com/mm/engine/config/WebClientConfig.java
+++ mq-service/src/main/java/com/mm/engine/config/WebClientConfig.java
@@ -1,15 +1,39 @@
 package com.mm.engine.config;
 
+import com.amazonaws.xray.AWSXRay;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
     @Bean
-    @LoadBalanced
-    public WebClient.Builder webClientBuilder(){
-        return WebClient.builder();
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder()
+                .filter(addXRayTraceHeader()); // Add X-Ray trace propagation filter
     }
+
+    private ExchangeFilterFunction addXRayTraceHeader() {
+        return (request, next) -> {
+            // Get the current trace header
+            String traceHeader = AWSXRay.getCurrentSegmentOptional()
+                    .map(segment -> segment.getTraceId().toString())
+                    .orElse(null);
+
+            if (traceHeader != null) {
+                // Add the trace header to the outgoing request
+                ClientRequest modifiedRequest = ClientRequest.from(request)
+                        .header("X-Amzn-Trace-Id", traceHeader)
+                        .build();
+                return next.exchange(modifiedRequest);
+            }
+
+            return next.exchange(request);
+        };
+    }
+
+
 }
