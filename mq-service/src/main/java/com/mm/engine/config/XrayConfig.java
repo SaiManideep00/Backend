79876a8eea00b9f50a87a09343f0841aeffd8541
SaiManideep00 <saimanideep00@gmail.com>
2025-01-09 16:41:42 -0500

    Added TraceId to BackendService

--- mq-service/src/main/java/com/mm/engine/config/XrayConfig.java
+++ mq-service/src/main/java/com/mm/engine/config/XrayConfig.java
@@ -1,7 +1,10 @@
 package com.mm.engine.config;
 
+import com.amazonaws.xray.AWSXRay;
 import com.amazonaws.xray.jakarta.servlet.AWSXRayServletFilter;
+import com.amazonaws.xray.slf4j.SLF4JSegmentListener;
 import com.amazonaws.xray.strategy.jakarta.SegmentNamingStrategy;
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,5 +17,10 @@ public class XrayConfig {
     public Filter TracingFilter()
     {
         return new AWSXRayServletFilter("MQ-Service");
+    }
+    @PostConstruct
+    public void init() {
+
+        AWSXRay.getGlobalRecorder().addSegmentListener(new SLF4JSegmentListener("AWS-XRAY-TRACE-ID"));
     }
 }
